20fe304ba9cbee59d73c82914a0d0533f3a2df3d
geriadam <geriadam04@gmail.com>
2024-12-04 02:50:15 +0700

    fix: fetch api with include params

--- middleware/fetchPropertyContent.js
+++ middleware/fetchPropertyContent.js
@@ -3,8 +3,11 @@ export default defineNuxtRouteMiddleware(async (to) => {
   const slug = to.params.slug;
   const propertyId = extractPropertyId(slug);
 
+  // Extract includes from meta if provided, or default to an empty array
+  const includes = to.meta.includes || [];
+
   if (propertyId) {
-    await propertyContentStore.fetchPropertyContent(propertyId);
+    await propertyContentStore.fetchPropertyContent(propertyId, includes);
   } else {
     console.error('Invalid slug format');
   }
--- stores/propertyContent.js
+++ stores/propertyContent.js
@@ -9,23 +9,31 @@ export const usePropertyContentStore = defineStore('propertyContent', {
     properties: {},
   }),
   actions: {
-    async fetchPropertyContent(id) {
+    async fetchPropertyContent(id, includes = []) {
       if (this.properties[id]) {
         return this.properties[id];
       }
 
       try {
+        const params = {
+          id,
+          ...(includes.length > 0 && { include: includes }),
+        };
+
         const { data, error: fetchError } = await useFetch(
           `${API_BASE_URL}/property/content`, {
-          params: {
-            id,
-            include: 'room',
-          },
+          params,
         });
 
         if (data.value) {
+          // Save the fetched data in the store
           this.properties[id] = data.value[id];
           return data.value[id];
+        }
+
+        if (fetchError) {
+          console.error('Error fetching property data:', fetchError);
+          throw fetchError;
         }
       } catch (error) {
         console.error('Failed to fetch property data:', error);
