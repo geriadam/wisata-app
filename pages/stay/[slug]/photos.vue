d8f9d07e7b9f5c249247a796d10d701167217ae5
geriadam <geriadam04@gmail.com>
2024-12-04 03:42:51 +0700

    fix: photos tab with image params

--- pages/stay/[slug]/photos.vue
+++ pages/stay/[slug]/photos.vue
@@ -1,24 +1,24 @@
 <template>
   <div>
-    <v-row :no-gutters="!$device.isDesktop" v-if="filteredImages.length > 0">
-      <v-col cols="4" v-for="(image, index) in filteredImages" :key="index">
+    <v-row :no-gutters="!$device.isDesktop" v-if="propertyData && propertyData.image &&propertyData.image.length > 0">
+      <v-col cols="4" v-for="(image, index) in propertyData.image" :key="index">
         <a @click="handleOpenModal">
           <v-sheet rounded="0" elevation="0">
             <div class="bg-transparent overflow-hidden aspect-ratio"
               style="height: 0px; width: 100%; padding-bottom: 100%;">
-              <img :alt="image.caption || 'Image'" 
-              :src="image.links['1000px']['href']" 
-                class="w-full h-full">
+              <img onerror="this.onerror=null; this.src='https://project-exterior-technical-test-app.up.railway.app/img/fallback-global.png';"  :alt="image.caption || 'Image'" :src="image.url.lg" class="w-full h-full">
             </div>
           </v-sheet>
           </a>
       </v-col>
     </v-row>
-    <v-dialog max-width="1124" v-model="openModal" v-if="filteredImages.length > 0">
+    <v-dialog max-width="1124" v-model="openModal" v-if="propertyData && propertyData.image && propertyData.image.length > 0">
       <v-carousel hide-delimiters>
           <v-carousel-item
-            v-for="(image, index) in filteredImages"
-            :src="image.links['1000px']['href']" 
+            v-for="(image, index) in propertyData.image"
+            onerror="this.onerror=null; this.src='https://project-exterior-technical-test-app.up.railway.app/img/fallback-global.png';" 
+            :src="image.url.lg"
+            :key="index"
             cover
           ></v-carousel-item>
         </v-carousel>
@@ -29,6 +29,7 @@
 
 definePageMeta({
   middleware: 'fetch-property-content',
+  includes: ['image'],
 });
 
 const route = useRoute();
@@ -37,40 +38,13 @@ const slug = route.params.slug;
 const propertyContentStore = usePropertyContentStore();
 const propertyId = extractPropertyId(slug);
 const propertyData = computed(() => propertyContentStore.properties[propertyId] || {});
-const filteredImages = ref([]);
+
 const openModal = ref(false);
-
-updateFilteredImages(propertyData);
-
-function updateFilteredImages(propertyData) {
-  watch(
-    () => propertyData.value,
-    (newValue) => {
-      if (isValidRoomData(newValue.room)) {
-        filteredImages.value = extractHeroImages(newValue.room);
-      } else {
-        filteredImages.value = [];
-      }
-    },
-    { immediate: true }
-  );
-}
 
 function handleOpenModal() {
   openModal.value = true;
 }
 
-function isValidRoomData(room) {
-  return room && typeof room === 'object';
-}
-
-function extractHeroImages(roomData) {
-  return Object.values(roomData).flatMap((room) =>
-    room.images
-      ? room.images.filter((image) => image.hero_image).map((image) => ({ ...image, room }))
-      : []
-  );
-}
 </script>
 <style scoped>
 .aspect-ratio {
